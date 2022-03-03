cbfcf01fe0f3aaf5317afac17ec7e887c762f6bb
gustavo <gustavo.isaac@gmail.com>
2022-03-03 18:06:51 +0000

    Login - RegisterForm

--- app/src/main/java/com/artemissoftware/hephaestusui/ui/login/RegistrationScreen.kt
+++ app/src/main/java/com/artemissoftware/hephaestusui/ui/login/RegistrationScreen.kt
@@ -1,24 +1,35 @@
 package com.artemissoftware.hephaestusui.ui.login
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.artemissoftware.hephaestusui.ui.login.composables.LoginMenu
 import com.artemissoftware.hephaestusui.ui.login.composables.NavigationBar
+import com.artemissoftware.hephaestusui.ui.login.composables.RegisterForm
 import com.artemissoftware.hephaestusui.ui.theme.LoginJetpackComposeTheme
 
+@ExperimentalFoundationApi
 @Composable
 fun RegistrationScreen() {
 
-    LazyColumn{
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ){
 
         item {
 
             Column {
 
                 NavigationBar()
-//                RegisterForm()
+                RegisterForm()
 //                RegisterOptions()
             }
 
@@ -28,6 +39,7 @@ fun RegistrationScreen() {
 
 }
 
+@ExperimentalFoundationApi
 @Preview(showBackground = true)
 @Composable
 private fun DefaultPreview() {
--- app/src/main/java/com/artemissoftware/hephaestusui/ui/login/composables/RegisterForm.kt
+++ app/src/main/java/com/artemissoftware/hephaestusui/ui/login/composables/RegisterForm.kt
@@ -1,7 +1,9 @@
 package com.artemissoftware.hephaestusui.ui.login.composables
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -9,6 +11,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
@@ -16,6 +20,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @ExperimentalFoundationApi
 @Composable
@@ -32,19 +37,13 @@ fun RegisterForm(){
 
     val focusManager = LocalFocusManager.current
 
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
 
-        TransparentTextField(
-            textFieldValue = emailValue,
-            textLabel = "Name",
-            keyboardType = KeyboardType.Text,
-            keyboardActions = KeyboardActions(
-                onNext = {
-                    focusManager.moveFocus(FocusDirection.Down)
-                }
-            ),
-            imeAction = ImeAction.Next
-        )
 
         TransparentTextField(
             textFieldValue = nameValue,
