d71f2417df604f495bc1bc8b9e613626776297c7
gustavo <gustavo.isaac@gmail.com>
2022-03-31 17:30:49 +0100

    onboarding - OnboardingNavigation

---
+++ app/src/main/java/com/artemissoftware/hephaestusui/ui/onboarding/navigation/OnboardingNavigation.kt
@@ -0,0 +1,29 @@
+package com.artemissoftware.hephaestusui.ui.onboarding.navigation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.artemissoftware.hephaestusui.ui.onboarding.screens.WelcomeScreen
+import com.google.accompanist.pager.ExperimentalPagerApi
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@Composable
+fun OnboardingNavigation(
+    navController: NavHostController,
+    startDestination: String
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(route = Screen.Welcome.route) {
+            WelcomeScreen(navController = navController)
+        }
+        composable(route = Screen.Home.route) {
+            //--HomeScreen()
+        }
+    }
+}
--- app/src/main/java/com/artemissoftware/hephaestusui/ui/onboarding/screens/WelcomeScreen.kt
+++ app/src/main/java/com/artemissoftware/hephaestusui/ui/onboarding/screens/WelcomeScreen.kt
@@ -1,18 +1,72 @@
 package com.artemissoftware.hephaestusui.ui.onboarding.screens
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.artemissoftware.hephaestusui.Greeting
-import com.artemissoftware.hephaestusui.ui.theme.HephaestusUITheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.artemissoftware.hephaestusui.ui.onboarding.composables.PagerScreen
+import com.artemissoftware.hephaestusui.ui.onboarding.models.OnBoardingPage
+import com.google.accompanist.pager.*
 
+@ExperimentalPagerApi
 @Composable
-fun WelcomeScreen() {
+fun WelcomeScreen(
+    navController: NavHostController,
+    welcomeViewModel: WelcomeViewModel = hiltViewModel()
+) {
 
+    val pages = listOf(
+        OnBoardingPage.First,
+        OnBoardingPage.Second,
+        OnBoardingPage.Third
+    )
+    val pagerState = rememberPagerState(pageCount = pages.size)
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        HorizontalPager(
+            modifier = Modifier.weight(10f),
+            state = pagerState,
+            verticalAlignment = Alignment.Top
+        ) { position ->
+            PagerScreen(onBoardingPage = pages[position])
+        }
+//        HorizontalPagerIndicator(
+//            modifier = Modifier
+//                .align(Alignment.CenterHorizontally)
+//                .weight(1f),
+//            pagerState = pagerState
+//        )
+//        FinishButton(
+//            modifier = Modifier.weight(1.3f),
+//            pagerState = pagerState
+//        ) {
+//            welcomeViewModel.saveOnBoardingState(completed = true)
+//            navController.popBackStack()
+//            navController.navigate(Screen.Home.route)
+//        }
+    }
 }
 
 
+@ExperimentalPagerApi
 @Preview(showBackground = true)
 @Composable
 private fun DefaultPreview() {
-    WelcomeScreen()
+    WelcomeScreen(hiltViewModel())
 }
