82030b7942de752149015b959345e5d11c0cf9ab
gustavo <gustavo.isaac@gmail.com>
2022-04-22 19:52:59 +0100

    people - PeopleRepository

---
+++ app/src/main/java/com/artemissoftware/hephaestusui/ui/people/data/repository/PeopleRepository.kt
@@ -0,0 +1,25 @@
+package com.artemissoftware.hephaestusui.ui.people.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.artemissoftware.hephaestusui.ui.people.data.dto.models.PersonDto
+import com.artemissoftware.hephaestusui.ui.people.data.pagingsource.GetPeoplePagingSource
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class PeopleRepository @Inject constructor(
+    private val getPeoplePagingSource: GetPeoplePagingSource
+){
+
+    fun getPeople(): Flow<PagingData<PersonDto>> {
+
+        return Pager(
+            config = PagingConfig(pageSize = 6),
+            pagingSourceFactory ={
+                getPeoplePagingSource
+            }
+        ).flow
+
+    }
+}
