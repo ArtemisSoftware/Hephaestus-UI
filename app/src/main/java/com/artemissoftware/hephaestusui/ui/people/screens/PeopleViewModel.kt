086bfdfabe386f6bd129d4a53d7d1813d5cd4cf8
gustavo <gustavo.isaac@gmail.com>
2022-04-23 15:58:46 +0100

    people - PeopleViewModel

--- app/src/main/java/com/artemissoftware/hephaestusui/ui/people/screens/PeopleViewModel.kt
+++ app/src/main/java/com/artemissoftware/hephaestusui/ui/people/screens/PeopleViewModel.kt
@@ -1,8 +1,21 @@
 package com.artemissoftware.hephaestusui.ui.people.screens
 
+import androidx.lifecycle.ViewModel
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.artemissoftware.hephaestusui.ui.people.data.dto.models.PersonDto
+import com.artemissoftware.hephaestusui.ui.people.domain.usecases.GetPeopleUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+
+import androidx.lifecycle.viewModelScope
 
 
 @HiltViewModel
-class PeopleViewModel() {
+class PeopleViewModel(
+    private val getPeopleUseCase: GetPeopleUseCase
+): ViewModel() {
+
+    val people: Flow<PagingData<PersonDto>> = getPeopleUseCase().cachedIn(viewModelScope)
+
 }
